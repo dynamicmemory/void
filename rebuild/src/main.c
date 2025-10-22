80bf9771ae2ce35beb1f19efe4d18e2d604bc4aa
dynamicmemory <joshua.hahn@live.com>
2025-10-22 19:19:24 +1000

    Started building cursor logic, half way through currently
    Fixed adding cursor position to end of render buffer + added input for
    the four different arrows.

--- rebuild/src/main.c
+++ rebuild/src/main.c
@@ -14,7 +14,12 @@ typedef struct editor {
     int col;
 } editor;
 
-void render_display(document *d);
+void init_editor(editor *e) {
+    e->row = 0;
+    e->col = 0;
+}
+
+void render_display(document *d, editor *e);
 
 int main(int argc, char *argv[]) {
     // Set the terminal into raw mode 
@@ -29,14 +34,34 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Failed to correctly handle file");
         return -1;
     }
+
+    editor e;
+    init_editor(&e);
     
-    render_display(&d);
+    // render_display(&d, &e);
 
     int c;
     while ((c=getc(stdin)) != EOF) {
+        render_display(&d, &e);
         if (c == CTRL_QUIT) exit(0);
         if (c == CTRL_SAVE) exit(0);  // TODO: Change to save shortly
-       
+         
+        // Cursor related code 
+        if (c == '\x1b') {
+            char two_chars[2];
+            if (read(0, two_chars, 1) == 0) continue;
+            if (read(0, two_chars + 1, 1) == 0) continue;
+
+            if (two_chars[0] == '[') {
+                switch(two_chars[1]) {
+                case'A': e.row = e.row - 1;
+                case'B': e.row = e.row + 1;
+                case'C': e.col = e.col + 1;
+                case'D': e.col = e.col - 1;
+                }
+            }
+        }
+        printf("value of row: %d, col %d\n", e.row, e.col);
         
     }
 
@@ -55,7 +80,7 @@ int main(int argc, char *argv[]) {
 }
 
 // TODO: ADD ERROR CHECKING AND HANDLING TO THIS
-void render_display(document *d) {
+void render_display(document *d, editor *e) {
     long capacity = 256;
     char *buffer= malloc(capacity);
 
@@ -85,11 +110,16 @@ void render_display(document *d) {
         size++;
     }
 
-    // Chars for unhiding the cursor
-    char *showc = "\x1b[?25h";
-    int showlen = strlen(showc);
-    memcpy(buffer + size, showc, showlen);
-    size += showlen;
+    // Make a string for moving the cursor and unhiding it and add it to the end 
+    int n = snprintf(buffer + size, capacity - size, "\x1b[%d;%d\x1b[?25h", e->row, e->col);
+    // If n is less then cap - size, then there wasn't enough space to write full string
+    if (n >= capacity - size) {
+        // Double cap size and rewrite string again
+        capacity *= 2;
+        n = snprintf(buffer + size, capacity - size, "\x1b[%d;%d\x1b[?25h", e->row, e->col);
+    }
+    // Assign n to the size of the buffer and away we go
+    size += n;
 
     write(STDOUT_FILENO, buffer, size);
 }
